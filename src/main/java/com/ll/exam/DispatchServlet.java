b5b41e74027881a5ea1e95fc9fc0a8c0c5854f66
Young998904 <like0998904@gmail.com>
2022-07-28 00:13:49 +0900

    Modify articleController.deDelete form GET to POST

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -28,9 +28,6 @@ public class DispatchServlet extends HttpServlet {
                     case "/usr/article/detail" :
                         articleController.showDetail(rq);
                         break;
-                    case "/usr/article/delete":
-                        articleController.doDelete(rq);
-                        break;
                     case "/usr/article/list":
                         articleController.showList(rq);
                         break;
@@ -46,6 +43,9 @@ public class DispatchServlet extends HttpServlet {
                 switch (rq.getActionPath()) {
                     case "/usr/article/write":
                         articleController.doWrite(rq);
+                        break;
+                    case "/usr/article/delete":
+                        articleController.doDelete(rq);
                         break;
                     case "/usr/article/modify":
                         articleController.doModify(rq);
